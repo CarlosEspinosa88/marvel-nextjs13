ccef7f5f912a599886254fd9fa2c1f1672080c6f
Carlos Espinosa <carloespinosa88@gmail.com>
2023-04-21 00:01:21 -0500

    some fixes and add fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,21 @@
 import Link from "next/link"
+import { Repository } from '../types'
 import { getCharacters } from '../utils/getCharacters'
 
 export default async function Page() {
-  const { data, statusResponse, statusCode } = await getCharacters()
+  const { data, status, code }: Repository = await getCharacters()
 
   return (
     <div>
       <h1>Hello, Home!</h1>
-      {statusResponse === 'Ok' && statusCode === 200 ? (
-        <div>
-          {data.map((character) => (
-            <div key={character.id}>
-              <Link href={`/character/${character.id}`}>
-                <h3>{character.id} / {character.name}</h3>
-              </Link>
-            </div>
-          ))}
-        </div>
+      {status === 'Ok' && code === 200 ? (
+        data.results.map((character) => (
+          <div key={character.id}>
+            <Link href={`/character/${character.id}`}>
+              <h3>{character.id} / {character.name}</h3>
+            </Link>
+          </div>
+        ))
       ) : (
         <h1>Unexpected error!</h1>
       )}
--- config/constants.tsx
+++ config/constants.tsx
@@ -2,7 +2,7 @@
 const CHARACTER_DATA = 'characters'
 
 export const TS = 1
-export const LIMIT = 10;
+export const LIMIT = 100;
 export const OFFSET = 0;
 export const QUERY = CHARACTER_DATA
 export const URL = process.env.MARVEL_URL
