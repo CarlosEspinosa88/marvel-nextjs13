2e262ee7f412f1f80eb64780de3d3c7728dc47bf
Carlos Espinosa <carloespinosa88@gmail.com>
2023-04-24 15:13:53 -0500

    add some styles with module css

--- app/character/[id]/page.tsx
+++ app/character/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { CharacterId, Repository } from '../../../types'
 import { getCharacterDetail } from '@utils/getCharacterDetail'
 import BoxCard from '@components/BoxCard'
+import styles from './character.module.css'
 
 
 export default async function Page({ params }: CharacterId) { 
@@ -9,7 +10,7 @@ export default async function Page({ params }: CharacterId) {
   return status === 'Ok' && code === 200 ? (
     <>
       {data.results.map((character) => (
-        <div key={character.id}>
+        <div key={character.id} className={styles.container}>
           <BoxCard
             id={character.id}
             name={character.name}
--- components/BoxCard/BoxCard.tsx
+++ components/BoxCard/BoxCard.tsx
@@ -1,17 +1,23 @@
 import { Character } from '../../types'
+import styles from './BoxCard.module.css'
 
 function BoxCard({ id, name, thumbnail }: Character) {
   return (
-    <>
-      <h1>Hello, {name}!</h1>
-      <img
-        width="100%"
-        loading='lazy'
-        decoding="async"
-        alt={`${id}-${name}`}
-        src={`${thumbnail.path}.${thumbnail.extension}`} 
-      />
-    </>
+    <div className={styles.container}>
+      <div className={styles.container_image}>
+        <img
+          width="100%"
+          loading='lazy'
+          decoding="async"
+          alt={`${id}-${name}`}
+          className={styles.image}
+          src={`${thumbnail.path}.${thumbnail.extension}`} 
+          />
+      </div>
+      <div className={styles.container_info}>
+        <h3 className={styles.title}>Hello, {name}!</h3>
+      </div>
+    </div>
   ) 
 }
 
