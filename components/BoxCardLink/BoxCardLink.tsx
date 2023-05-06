7fac8b0cb9bd0fd02ecc3d2f98c8052aa4af7012
Carlos Espinosa <carloespinosa88@gmail.com>
2023-05-06 10:54:08 -0500

    add a simple filter to search characters

--- components/BoxCardLink/BoxCardLink.tsx
+++ components/BoxCardLink/BoxCardLink.tsx
@@ -1,19 +1,36 @@
 'use client'
 
-import { useEffect, useState, useRef } from "react"
 import Link from "next/link"
+import { useEffect, useState, useRef, ChangeEvent, useMemo } from "react"
 import { Character, Results } from '../../types'
+import SearchBox from "@components/SearchBox"
 import styles from './BoxCardLink.module.css'
 
 function BoxCardLink({ results }: Results) {
   const node = useRef<HTMLDivElement>(null)
   const [characters, setCharacters] = useState<Character[]>([])
+  const [inputValue, setInputValue] = useState<string>('')
+
+  function handleInputValue(event: ChangeEvent<HTMLInputElement>) {
+    setInputValue(event.target.value) 
+  }
+
+  const filtereChar = useMemo(() => {
+    return characters.filter((heroe) => 
+      heroe.name.toLowerCase() === inputValue.toLowerCase()
+    )
+
+  }, [characters, inputValue, results])
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          setCharacters(results)
+          if (filtereChar.length > 0 ) {
+            setCharacters(filtereChar)
+          } else {
+            setCharacters(results)
+          }
         }
       })
     })
@@ -24,31 +41,34 @@ function BoxCardLink({ results }: Results) {
 
     return () => observer.disconnect()
 
-  }, [results])
+  }, [results, filtereChar])
 
   return (
-    <div ref={node} className={styles.grid_container}>
-      {characters.map((character) => (
-        <div key={character.id} className={styles.image_container}>
-          <Link href={`/character/${character.id}`}>
-            <div className={styles.image_wrapper}>
-              <img
-                width="100%"
-                loading='lazy'
-                decoding="async"
-                alt={character.name}
-                className={styles.image}
-                src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-                />
-            </div>
-            <div className={styles.info_container}>
-              <h4 className={styles.title}>
-                {character.name}
-              </h4>
-            </div>
-          </Link>
-        </div>
-      ))}
+    <div>
+      <SearchBox handleInputValue={handleInputValue} inputValue={inputValue} />
+      <div ref={node} className={styles.grid_container}>
+        {characters.map((character) => (
+          <div key={character.id} className={styles.image_container}>
+            <Link href={`/character/${character.id}`}>
+              <div className={styles.image_wrapper}>
+                <img
+                  width="100%"
+                  loading='lazy'
+                  decoding="async"
+                  alt={character.name}
+                  className={styles.image}
+                  src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+                  />
+              </div>
+              <div className={styles.info_container}>
+                <h4 className={styles.title}>
+                  {character.name}
+                </h4>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   )
   
--- components/SearchBox/SearchBox.tsx
+++ components/SearchBox/SearchBox.tsx
@@ -1,26 +1,13 @@
-'use client'
-
-import { useState, ChangeEvent } from 'react'
-import { Character } from '../../types'
-import useGetCharacterInput from '../../hooks/useGetCharacterInput'
 import Input from '@components/Input'
+import { InputValues } from '../../types'
 
-type SearchBoxTypes = {
-  results: Character[]
-}
 
-function SearchBox() {
-  const [ data, setData ] = useState<string>('')
-
-  function handleInputValue(event: ChangeEvent<HTMLInputElement>) {
-    setData(event.target.value) 
-  }
-
+function SearchBox({ handleInputValue, inputValue }: InputValues) {
   return (
     <div>
       <Input 
         id="123"
-        value={data}
+        value={inputValue}
         label='Search'
         maxLength={10}
         placeholder="Write here you heroe"
